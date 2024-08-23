445152c02514bb018fb68ab70b26a55e08e2abc6
Tomsar95 <107140629+Tomsar95Laika@users.noreply.github.com>
2024-08-23 18:30:57 -0500

    fitment fix

--- lib/features/cats/presentation/widgets/cats/cats_list_widget.dart
+++ lib/features/cats/presentation/widgets/cats/cats_list_widget.dart
@@ -34,10 +34,25 @@ class CatsList extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      cat.name,
-                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          cat.name,
+                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              'Más...',
+                              style: TextStyle(color: CustomColors.black),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Image.network(
@@ -45,26 +60,17 @@ class CatsList extends StatelessWidget {
                     width: double.infinity,
                     height: 200,
                     fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) => const Icon(Icons.error),
+                    errorBuilder: (context, error, stackTrace) => Center(child: const Icon(Icons.error)),
                   ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Text('Pais de origen: ${cat.origin ?? "Desconocido"}'),
                         Text('Inteligencia: ${cat.intelligence ?? "Desconocido"}'),
                       ],
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        'Más...',
-                        style: TextStyle(color: CustomColors.black),
-                      ),
                     ),
                   ),
                 ],
